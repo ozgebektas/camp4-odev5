9cd1cb9cda4821e874488f50f71fe7597c27c32c
Özge Bektaş <ozgebek.dev@gmail.com>
2023-02-28 12:33:29 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camp4_odev5
+{
+    class MyDictionary<Key,Value>
+    {
+
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+        
+        public void AddDictionary(Key key,Value value)
+        {
+            Key[] tempArrayKey = keys;
+            keys = new Key[keys.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[0] = tempArrayKey[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            Value[] tempArrayValue = values;
+            values = new Value[values.Length + 1];
+            for (int x = 0; x < tempArrayValue.Length; x++)
+            {
+                values[0] = tempArrayValue[x];
+            }
+            values[values.Length - 1] = value;
+            Console.WriteLine(keys[keys.Length - 1]+" " +values[values.Length-1]);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Camp4_odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> vocabulary = new MyDictionary<int ,string>();
+            vocabulary.AddDictionary(1,"özge");
+            vocabulary.AddDictionary(2,"ezgi");
+            vocabulary.AddDictionary(3,"berkay");
+        }
+    }
+}
